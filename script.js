9de29b124f9dc73e1f6feb82ec9a89d66f12477d
prajwal2103 <137304916+prajwal2103@users.noreply.github.com>
2023-12-04 19:36:45 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
- let a = prompt()
-  let b= prompt();
+ let a = prompt("Enter the first number:")
+  let b= prompt("Enter the second number:");
   let num1=parseInt(a);
   let num2=parseInt(b);
   // console.log(typeof num1 +" "+ typeof num2);
